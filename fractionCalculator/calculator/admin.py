f0c7258d46a237f6762e0b20f5307f417509834a
JoseRobertoGC <a01656294@tec.mx>
2023-03-30 12:49:32 -0600

    admin changes

--- fractionCalculator/calculator/admin.py
+++ fractionCalculator/calculator/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Jugadores
+from .models import Jugadores,Usuarios,Partidas
 
 # Register your models here.
 admin.site.register(Jugadores)
+admin.site.register(Usuarios)
+admin.site.register(Partidas)
 
